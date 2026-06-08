4eb66efbe2b92a3ebe353ef53ae6cf9311498a45
Administrator <admin@example.com>
2026-06-08 09:24:19 +0800

    chore: cleanup tmp files

--- deploy_netlify.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "C:\Users\Administrator\Documents\New project\4-24ciyuntu"
-echo [Netlify Deploy] Starting...
-npx netlify deploy --prod --dir=dist --functions=netlify/functions > netlify_deploy.log 2>&1
-echo [Netlify Deploy] Done. Check netlify_deploy.log
